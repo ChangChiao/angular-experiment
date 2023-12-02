599d3c65c8b3ef895cb9a5680ce1024f1e0a5c73
joe <JoeCT_Chang@moxa.com>
2023-12-02 09:43:18 +0800

    feat: add setup component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SetupComponent } from './components/setup.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, SetupComponent, RouterModule],
   selector: 'angular-experiment-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ src/app/components/setup.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+
+@Component({
+  selector: 'angular-experiment-setup',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  templateUrl: './setup.component.html',
+  styleUrl: './setup.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SetupComponent {
+  formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this.formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this.formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+}
