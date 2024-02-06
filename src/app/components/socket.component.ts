9e846b0dbffbd1a4831d7f1a8a9438593c98afdd
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-06 11:01:21 +0800

    feat: socket component

---
+++ src/app/components/socket.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { io } from 'socket.io-client';
+
+@Component({
+  selector: 'angular-experiment-socket',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>socket works!</p>`,
+  styles: ``,
+})
+export class SocketComponent {
+  socket = io('http://localhost:3000');
+
+  constructor() {
+    this.socket.on('error', (error) => {
+      console.log('error---', error);
+    });
+  }
+}
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -15,6 +15,7 @@ import { MatInputModule } from '@angular/material/input';
 import { DataImportComponent } from 'src/app/components/data-import/data-import.component';
 import { DraggableComponent } from 'src/app/components/draggable/draggable.component';
 import { SetupComponent } from 'src/app/components/setup/setup.component';
+import { SocketComponent } from 'src/app/components/socket.component';
 
 @Component({
   selector: 'angular-experiment-index',
@@ -24,6 +25,7 @@ import { SetupComponent } from 'src/app/components/setup/setup.component';
     SetupComponent,
     DataImportComponent,
     DraggableComponent,
+    SocketComponent,
     ReactiveFormsModule,
     MatInputModule,
   ],
@@ -38,6 +40,7 @@ import { SetupComponent } from 'src/app/components/setup/setup.component';
         <input matInput formControlName="projectName" />
         <button (click)="submit()">send</button>
       </form>
+      <angular-experiment-socket></angular-experiment-socket>
     </div>
   `,
   styleUrl: './index.component.scss',
