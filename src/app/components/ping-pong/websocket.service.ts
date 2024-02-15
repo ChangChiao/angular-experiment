34617f7c697e6757c3b6a7068f39e1fe759f137b
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-15 11:08:41 +0800

    feat: add rxjs websocket service

---
+++ src/app/components/ping-pong/websocket.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WebSocketSubject, webSocket } from 'rxjs/webSocket';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WebsocketService {
+  #subject$: WebSocketSubject<unknown> | null = null;
+  initialize(url: string): Observable<any> {
+    return new Observable((observer) => {
+      try {
+        this.#subject$ = webSocket(url);
+        const subscription = this.#subject$.asObservable().subscribe({
+          next: (msg) => {
+            console.log('message received: ' + msg);
+            observer.next(msg);
+          },
+          error: (err) => {
+            console.log('err', err);
+            observer.error(err);
+          },
+          complete: () => {
+            console.log('complete');
+            observer.complete();
+          },
+        });
+
+        return () => {
+          subscription.unsubscribe();
+          this.#subject$?.complete();
+        };
+      } catch (error) {
+        return observer.error(error);
+      }
+    });
+  }
+
+  sendMsg(msg: string) {
+    this.#subject$?.next(msg);
+  }
+}
