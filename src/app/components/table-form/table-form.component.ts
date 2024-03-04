f75e8f980f202731114792449715a1332082793b
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-03-04 15:44:57 +0800

    feat: add selection

--- src/app/components/table-form/table-form.component.ts
+++ src/app/components/table-form/table-form.component.ts
@@ -1,3 +1,4 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import {
@@ -7,6 +8,7 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { map } from 'rxjs';
@@ -16,20 +18,40 @@ import { ProductList, rowItem } from './model/table-form.model';
 @Component({
   selector: 'angular-experiment-table-form',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatPaginatorModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './table-form.component.html',
   styleUrl: './table-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableFormComponent implements OnInit, AfterViewInit {
   constructor(private http: HttpClient) {}
-  displayedColumns: string[] = ['id', 'price', 'rating', 'hash'];
+  displayedColumns: string[] = ['select', 'id', 'price', 'rating', 'hash'];
+  selection = new SelectionModel<rowItem>(true, []);
   tableDataSource = new MatTableDataSource<rowItem>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngAfterViewInit() {
     this.tableDataSource.paginator = this.paginator;
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.tableDataSource.data.length;
+    return numSelected == numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.tableDataSource.data.forEach((row) => this.selection.select(row));
   }
 
   getApiData() {
